d2799326bd6075ec64b6494ec557e5545355409a
Jullyana Melo <jullyanaglaucia@gmail.com>
2024-11-27 15:45:32 -0300

    fetch: Layout of keybord

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -25,22 +25,31 @@ class _FormScreenState extends State<FormScreen> {
         backgroundColor: Colors.purple,
       ),
       body: Center(
-        child: Container(
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 254, 242, 255),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
+        child: SingleChildScrollView(
+          child: Container(
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 254, 242, 255),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    validator: (String? value) {
+                      if (value != null && value.isEmpty) {
+                        return "Insira o nome da tarefa";
+                      }
+                      return null;
+                    },
                     controller: nameController,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText:'Usuário',
+                        labelText: 'Usuário',
                         fillColor: Colors.white70,
                         filled: true),
                   ),
@@ -48,10 +57,20 @@ class _FormScreenState extends State<FormScreen> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          int.parse(value) > 5 ||
+                          int.parse(value) < 1) {
+                        return "Insira a dificuldade da tarefa";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.number,
                     controller: difficultyController,
                     decoration: const InputDecoration(
-                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black12)),
-                        labelText:'Dificuldade' ,
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black12)),
+                        labelText: 'Dificuldade',
                         fillColor: Colors.white70,
                         filled: true),
                   ),
@@ -59,6 +78,10 @@ class _FormScreenState extends State<FormScreen> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    keyboardType: TextInputType.url,
+                    onChanged: (text) {
+                      setState(() {});
+                    },
                     controller: imageController,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
@@ -77,11 +100,24 @@ class _FormScreenState extends State<FormScreen> {
                   ),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(10),
-                    child: Image.network(imageController.text, fit: BoxFit.cover,),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stacktrace) {
+                        return Image.asset('assets/images/Img_box@2x.png');
+                      },
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                  ElevatedButton(onPressed: (){print(nameController.text);},child: const Text("Adicionar"),),
+                ElevatedButton(
+                  onPressed: () {
+                    print(nameController.text);
+                  },
+                  child: const Text("Adicionar"),
+                ),
               ],
+            ),
           ),
         ),
       ),
